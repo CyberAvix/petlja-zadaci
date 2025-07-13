d126f6b4bdf6f3ff1ba994874e60692cfc87b61b
Luka Stojanovic <stojanoviclu@gmail.com>
2025-07-13 21:01:55 +0200

    Initial commit

---
+++ aritmetika.py
@@ -0,0 +1,51 @@
+import math
+
+Ax = float(input("kordinata x za A"))
+Ay = float(input("kordinata y za A"))
+Bx = float(input("kordinata x za B"))
+By = float(input("kordinata y za B"))
+
+u = math.sqrt((Bx - Ax)**2 + (By - Ay)**2)
+print(format(u, '.5f'))
+
+# d = int(input())
+# s = int(input())
+# r = int(input())
+
+# do = d + 2*r
+# so = s + 2*r
+
+# print(2*do + 2*so)
+
+
+# import math
+
+
+# povrsina = float(input())
+
+# r = math.sqrt(povrsina/math.pi)
+
+# print(2*r*math.pi)
+
+
+
+
+
+# reci = input().split()
+# v = int(reci[0])
+# s = int(reci[1])
+
+# p = v * s
+# print(f"Povrsina je {p} mm^2")
+
+
+
+
+
+# d = int(input("Unesi duzinu"))
+# s = int(input("Unesi sirinu"))
+
+# def kolikoPretrci(d, s):
+#     return(2*d + 2*s)
+
+# print(kolikoPretrci(d,s))    
